82b64ad8e68cc8572b91686230477d6eec7e602c
alexdev <alexdev.workenv@gmail.com>
2025-04-17 23:36:38 -0400

    feature(packages/api): add error interceptor for normalize error responses

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,12 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { ResponseInterceptor } from './common/interceptors/response.interceptor'
+import { ErrorInterceptor } from './common/interceptors/error.interceptor'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new ResponseInterceptor());
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalInterceptors(new ResponseInterceptor(), new ErrorInterceptor())
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
