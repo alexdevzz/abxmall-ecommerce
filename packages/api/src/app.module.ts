ff5242bb6f925d26f3dc235d73509dc625c812cb
alexdev <alexdev.workenv@gmail.com>
2025-04-17 23:46:44 -0400

    fix(packages/api): refractor code with the prettier config change

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,12 +1,9 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ProductsModule } from './modules/products/products.module';
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ProductsModule } from './modules/products/products.module'
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-ecommerce-db'),
-    ProductsModule,
-  ],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nest-ecommerce-db'), ProductsModule],
   controllers: [],
   providers: [],
 })
--- packages/api/src/modules/products/products.controller.ts
+++ packages/api/src/modules/products/products.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ProductsService } from './products.service';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
-import { ResponseMetadata } from 'src/common/decorators/response.decorator';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { ProductsService } from './products.service'
+import { CreateProductDto } from './dto/create-product.dto'
+import { UpdateProductDto } from './dto/update-product.dto'
+import { ResponseMetadata } from 'src/common/decorators/response.decorator'
 
 @Controller('products')
 export class ProductsController {
@@ -11,30 +11,30 @@ export class ProductsController {
   @Post()
   @ResponseMetadata('product created successfully')
   create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+    return this.productsService.create(createProductDto)
   }
 
   @Get()
   @ResponseMetadata('all products retrieved successfully')
   findAll() {
-    return this.productsService.findAll();
+    return this.productsService.findAll()
   }
 
   @Get(':id')
   @ResponseMetadata('product retrieved successfully')
   findOne(@Param('id') id: string) {
-    return this.productsService.findOneById(id);
+    return this.productsService.findOneById(id)
   }
 
   @Patch(':id')
   @ResponseMetadata('product updated successfully')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.updateOneById(id, updateProductDto);
+    return this.productsService.updateOneById(id, updateProductDto)
   }
 
   @Delete(':id')
   @ResponseMetadata('product deleted successfully')
   remove(@Param('id') id: string) {
-    return this.productsService.removeOneById(id);
+    return this.productsService.removeOneById(id)
   }
 }
