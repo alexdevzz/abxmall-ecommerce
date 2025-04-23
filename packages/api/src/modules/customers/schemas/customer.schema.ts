be56fea501e40ebf51c965177d5e017e54ce742b
alexdev <alexdev.workenv@gmail.com>
2025-04-23 18:35:42 -0400

    feature(packages/api): create customer module and create basic schema

---
+++ packages/api/src/modules/customers/schemas/customer.schema.ts
@@ -0,0 +1,58 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose'
+
+@Schema({
+  timestamps: true,
+})
+export class Customer {
+  @Prop({
+    required: true,
+    minlength: 7,
+    maxlength: 50,
+    unique: true,
+    trim: true,
+  })
+  email: string
+
+  @Prop({
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+    trim: true,
+  })
+  name: string
+
+  @Prop({
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+    trim: true,
+  })
+  lastName: string
+
+  @Prop({
+    required: true,
+    minlength: 3,
+    maxlength: 500,
+    trim: true,
+  })
+  billingAddress: string
+
+  @Prop({
+    required: true,
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  })
+  country: string
+
+  @Prop({
+    required: true,
+    minlength: 6,
+    maxlength: 20,
+    trim: true,
+    NaN: false,
+  })
+  phone: string
+}
+
+export const CustomerSchema = SchemaFactory.createForClass(Customer)
