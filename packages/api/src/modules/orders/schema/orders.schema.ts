f45947519e5b628f4a23e68ad6fccd8e6e5bf01c
alexdev <alexdev.workenv@gmail.com>
2025-04-25 17:32:06 -0400

    feature(packages/api): create order module, basic schema and OrderStatus enum

---
+++ packages/api/src/modules/orders/schema/orders.schema.ts
@@ -0,0 +1,57 @@
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose'
+import { OrderStatus } from '../enum/order-status.enum'
+
+@Schema({
+  timestamps: true,
+})
+export class Order {
+  @Prop({
+    min: 0,
+  })
+  ammount: number
+
+  @Prop({
+    trim: true,
+    required: true,
+    minlength: 4,
+    maxlength: 500,
+  })
+  shippingAddress: string
+
+  @Prop({
+    trim: true,
+    required: true,
+    minlength: 4,
+    maxlength: 500,
+  })
+  orderAddress: string
+
+  @Prop({
+    trim: true,
+    required: true,
+    minlength: 7,
+    maxlength: 50,
+  })
+  orderEmail: string
+
+  @Prop({
+    trim: true,
+    required: true,
+    minlength: 4,
+    maxlength: 20,
+    enum: Object.values(OrderStatus),
+    default: OrderStatus.CREATED,
+  })
+  orderStatus: string
+
+  @Prop({
+    required: true,
+    minlength: 4,
+    maxlength: 10,
+    unique: true,
+    trim: true,
+  })
+  sku: string
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order)
